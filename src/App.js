35831b93cf518f4f688b7fa1d69a5571159a2d92
Harshavardhan <187y1a0575@gmail.com>
2021-01-06 00:43:59 +0530

    must try to add contacts... or something like that

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+//Input field to be pushed to the bottom of the page
+//when send is clicked, message must be displayed above the input
 import React, {useState} from 'react'
 import './App.css';
 
 function App() {
   const [sendMessage,setSendMessage]=useState('');
   let [displayMessage,setDisplayMessage]=useState('');
-  let [allMessages, setAllMessages]=useState([]);
+  let [messageArray,setMessageArray]=useState([]);
   function handleChange(e){
     e.preventDefault();
     setSendMessage(e.target.value);
+
   }
   function handleClick(){
     setDisplayMessage(sendMessage);
-    allMessages.push(displayMessage);
+    setMessageArray( arr => [...arr, `${displayMessage}`]);
     setSendMessage('');
   }
+
   return (
-    <div className="container">
-      <p>{allMessages}</p>
-      <input type="text" placeholder="Send a message" value={sendMessage} onChange={handleChange}/>
-      <button type="button" onClick={handleClick}>Send</button>
-    </div>
+    <body>
+      {
+        messageArray.map(message=>{
+          return <p>{message}</p>
+        })
+      }
+      <div className="form">
+        <input type="text" placeholder="Send a message" value={sendMessage} onChange={handleChange} required/>
+        <button type="button" onClick={handleClick}>Send</button>
+      </div>
+    </body>
   );
 }
 
