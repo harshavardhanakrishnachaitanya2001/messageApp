eb54e89daf5a4888c57d58f152523e7e84e087c0
Harshavardhan <187y1a0575@gmail.com>
2021-01-07 00:07:13 +0530

    Added some styles

--- src/App.js
+++ src/App.js
@@ -17,15 +17,23 @@ function App() {
     setMessageArray( arr => [...arr, `${displayMessage}`]);
     setSendMessage('');
   }
-
   return (
     <body>
-      {
-        messageArray.map(message=>{
-          return <p>{message}</p>
-        })
-      }
+      <nav>
+        <ul>
+          <li>Contacts</li>
+          <li>Chats</li>
+        </ul>
+      </nav>
+      <h1>
+      <img src='./images/icon.png' alt=""/>Connect to the world
+      </h1>
       <div className="form">
+        {
+          messageArray.map(message=>{
+            return <p>{message}</p>
+          })
+        }
         <input type="text" placeholder="Send a message" value={sendMessage} onChange={handleChange} required/>
         <button type="button" onClick={handleClick}>Send</button>
       </div>
